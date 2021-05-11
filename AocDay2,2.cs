803d6d17bfd969d099165108cd0ede348eeccae4
Devon Garber <75411562+devon-garber@users.noreply.github.com>
2021-05-11 14:03:59 -0700

    Add files via upload

---
+++ AocDay2,2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Aoc
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = System.IO.File.ReadLines("input2.1.txt").ToArray();
+
+            int evenlyDivisTotal = 0;
+            foreach (string line in input)
+            {
+                string[] split = line.Split('\t');
+                IEnumerable<int> lineConverted = split.Select(x => Int32.Parse(x));
+                foreach (int i in lineConverted)
+                {
+                    List<int> withoutI = lineConverted.ToList();
+                    withoutI.Remove(i);
+                    if (withoutI.Any(x => i % x == 0))
+                    {
+                        evenlyDivisTotal += i / withoutI.First(x => i % x == 0);
+                    }
+                }
+            }
+
+            Console.WriteLine(evenlyDivisTotal);
+        }
+    }
+}

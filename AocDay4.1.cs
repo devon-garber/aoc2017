88472c251f7855196bf70c5ee20ac6cd95b93ed4
Devon Garber <75411562+devon-garber@users.noreply.github.com>
2021-05-12 15:32:51 -0700

    Add files via upload

---
+++ AocDay4.1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Aoc
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int validPassphrases = 0;
+            foreach (string passphrase in System.IO.File.ReadLines("input4.1.txt"))
+            {
+                bool passphraseValid = true;
+                List<string> words = passphrase.Split(' ').ToList();
+                foreach (string word in words)
+                {
+                    if (words.Count(x => x == word) > 1)
+                    {
+                        passphraseValid = false;
+                    }
+                }
+
+                if (passphraseValid)
+                {
+                    validPassphrases++;
+                }
+            }
+
+            Console.WriteLine(validPassphrases);
+        }
+    }
+}

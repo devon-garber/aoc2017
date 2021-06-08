e9edf1d2146fc2de183d879513206375086f5d30
Devon Garber <75411562+devon-garber@users.noreply.github.com>
2021-06-08 15:39:56 -0700

    Add files via upload

---
+++ AocDay9.2.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Aoc
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = System.IO.File.ReadLines("input9.1.txt").ToArray();
+            if (input.Length != 1)
+            {
+                throw new InvalidProgramException();
+            }
+
+            int garbageTotal = 0;
+            char lastChar = '\0';
+            bool inGarbage = false;
+            foreach (char c in input.First())
+            {
+                if (lastChar == '!')
+                {
+                    lastChar = '\0';
+                    continue;
+                }
+
+                if (c == '<' && !inGarbage)
+                {
+                    inGarbage = true;
+                }             
+                else if (c == '>')
+                {
+                    inGarbage = false;
+                }
+                else if (inGarbage && c != '!')
+                {
+                    garbageTotal++;
+                }
+
+                lastChar = c;
+            }
+
+            Console.WriteLine(garbageTotal);
+        }
+    }
+}
